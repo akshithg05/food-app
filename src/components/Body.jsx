49d910a54b77abfa7a703e91752c2eda261cd83c
Akshith G <akshithg01@gmail.com>
2025-06-05 21:34:41 +0530

    fetching from swiggy api

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,23 @@
 import RestaurantCard from "./RestaurantCard";
 import data from "../utils/mock-data";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
-  const [isTopRestaurantsSelected, setIsTopRestaurantsSelected] = useState(false);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
-  const filteredRestaurants = !isTopRestaurantsSelected
-    ? data.restaurants
-    : data.restaurants.filter((res) => res.info.avgRating > 4.5);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.97530&lng=77.59100&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const jsonData = await data.json();
+
+    // Optional chaining
+    setListOfRestaurants(jsonData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+  }
 
   return (
     <div className="body">
@@ -16,16 +26,15 @@ const Body = () => {
           className="filter-btn"
           onClick={() => {
             // callback function which will be clicked on click
-            console.log("Button clicked");
-            setIsTopRestaurantsSelected(true);
+            setListOfRestaurants(listOfRestaurants.filter((res) => res.info.avgRating > 4.5));
           }}
         >
           Top rated restaurants (&gt; 4.5 stars)
         </button>
-        <button onClick={() => setIsTopRestaurantsSelected(false)}>Clear filter</button>
+        <button onClick={() => setListOfRestaurants(data?.restaurants)}>Clear filter</button>
       </div>
       <div className="res-container">
-        {filteredRestaurants.map((res) => (
+        {listOfRestaurants.map((res) => (
           <RestaurantCard key={res?.info?.id} resData={res} />
         ))}
       </div>
